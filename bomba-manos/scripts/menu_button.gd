c5f4d1b2175c69be576237ba9233a3c7f652da57
Messias Alves <messiasalvesdesousa098@gmail.com>
2026-01-25 15:05:30 -0300

    menu inicial completo

--- bomba-manos/scripts/menu_button.gd
+++ bomba-manos/scripts/menu_button.gd
@@ -1,8 +1,8 @@
-extends Node2D
+extends Control
 @onready var SelectPlayer := preload("res://scenes/PlayerSelector.tscn")
-func _on_pressed() -> void:
-	SelectPlayer.instace()
 
-
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_packed(SelectPlayer)
+	
 func _on_button_2_pressed() -> void:
-	pass
+	get_tree().quit()
