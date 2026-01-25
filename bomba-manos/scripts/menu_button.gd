5d5c3db439eeb4d1d9a3a5e5a5075a4250ac1722
Messias Alves <messiasalvesdesousa098@gmail.com>
2026-01-24 23:26:37 -0300

    menu inicial e menu selecionar player

--- bomba-manos/scripts/menu_button.gd
+++ bomba-manos/scripts/menu_button.gd
@@ -1,1 +1,8 @@
-extends Node
+extends Node2D
+@onready var SelectPlayer := preload("res://scenes/PlayerSelector.tscn")
+func _on_pressed() -> void:
+	SelectPlayer.instace()
+
+
+func _on_button_2_pressed() -> void:
+	pass
