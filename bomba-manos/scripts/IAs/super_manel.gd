1de14a1486be98a6a55217152bcfd0b7dc05dece
igor Sousa <igbk512@gmail.com>
2026-05-22 19:52:05 -0300

    padroes de ataque

--- bomba-manos/scripts/IAs/super_manel.gd
+++ bomba-manos/scripts/IAs/super_manel.gd
@@ -2,7 +2,11 @@ extends Node2D
 
 var projectile01Scene = preload("res://scenes/levels/campaign/extras/projectile_01.tscn")
 var canAttack := true
-var attackCooldown := 1.0
+var attackCooldown1 := 0.1
+var attackCooldown2 := 0.25
+var attackCooldown3 := 0.5
+var attackCooldown4 := 1.0
+var attackCooldown5 := 3.0
 	
 func _ready() -> void:
 	pass
@@ -11,17 +15,92 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 		if canAttack == true:
-			var phRandom = randi_range(1, 3)
+			var phRandom = randi_range(4, 4)
 			canAttack = false
 			match phRandom:
 				1:
-					attackTop($"attackSpawn/top/3")
+					attackZigzag()
 				2:
-					attackLeft($"attackSpawn/left/2")
+					attackLeft01()
 				3:
-					attackRight($"attackSpawn/right/2")
-			await get_tree().create_timer(attackCooldown).timeout
+					attackLeft02()
+				4:
+					attackCircle()
+				
+			await get_tree().create_timer(attackCooldown5).timeout
 			canAttack = true
+
+func attackZigzag():
+	attackTop($"attackSpawn/top/1")
+	await get_tree().create_timer(attackCooldown1).timeout
+	attackTop($"attackSpawn/top/2")
+	await get_tree().create_timer(attackCooldown1).timeout
+	attackTop($"attackSpawn/top/3")
+	await get_tree().create_timer(attackCooldown1).timeout
+	attackTop($"attackSpawn/top/4")
+	await get_tree().create_timer(attackCooldown1).timeout
+	attackTop($"attackSpawn/top/5")
+	await get_tree().create_timer(attackCooldown1).timeout
+	attackTop($"attackSpawn/top/4")
+	await get_tree().create_timer(attackCooldown1).timeout
+	attackTop($"attackSpawn/top/3")
+	await get_tree().create_timer(attackCooldown1).timeout
+	attackTop($"attackSpawn/top/2")
+	await get_tree().create_timer(attackCooldown1).timeout
+	attackTop($"attackSpawn/top/1")
+	await get_tree().create_timer(attackCooldown1).timeout
+	attackTop($"attackSpawn/top/2")
+	await get_tree().create_timer(attackCooldown1).timeout
+	attackTop($"attackSpawn/top/3")
+	await get_tree().create_timer(attackCooldown1).timeout
+	attackTop($"attackSpawn/top/4")
+	await get_tree().create_timer(attackCooldown1).timeout
+	attackTop($"attackSpawn/top/5")
+	
+func attackCircle():
+	attackLeft($"attackSpawn/left/3")
+	await get_tree().create_timer(attackCooldown1).timeout
+	attackLeft($"attackSpawn/left/2")
+	await get_tree().create_timer(attackCooldown1).timeout
+	attackLeft($"attackSpawn/left/1")
+	await get_tree().create_timer(attackCooldown1).timeout
+	attackLeft($"attackSpawn/top/1")
+	await get_tree().create_timer(attackCooldown1).timeout
+	attackLeft($"attackSpawn/top/2")
+	await get_tree().create_timer(attackCooldown1).timeout
+	attackLeft($"attackSpawn/top/3")
+	await get_tree().create_timer(attackCooldown1).timeout
+	attackLeft($"attackSpawn/top/4")
+	await get_tree().create_timer(attackCooldown1).timeout
+	attackLeft($"attackSpawn/top/5")
+	await get_tree().create_timer(attackCooldown1).timeout
+	attackLeft($"attackSpawn/right/1")
+	await get_tree().create_timer(attackCooldown1).timeout
+	attackLeft($"attackSpawn/right/2")
+	await get_tree().create_timer(attackCooldown1).timeout
+	attackLeft($"attackSpawn/right/3")
+	await get_tree().create_timer(attackCooldown1).timeout
+	
+	
+	
+	
+
+
+func attackLeft01():
+	attackLeft($"attackSpawn/left/1")
+	await get_tree().create_timer(attackCooldown2).timeout
+	attackLeft($"attackSpawn/left/2")
+	await get_tree().create_timer(attackCooldown2).timeout
+	attackLeft($"attackSpawn/left/3")
+	await get_tree().create_timer(attackCooldown2).timeout
+	
+func attackLeft02():
+	attackLeft($"attackSpawn/left/3")
+	await get_tree().create_timer(attackCooldown2).timeout
+	attackLeft($"attackSpawn/left/1")
+	await get_tree().create_timer(attackCooldown2).timeout
+	attackLeft($"attackSpawn/left/2")
+	await get_tree().create_timer(attackCooldown1).timeout
 
 
 
